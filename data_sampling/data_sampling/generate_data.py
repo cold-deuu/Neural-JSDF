351bd77aabdd92c268050d1b725aa367ca5d1146
cold-deuu <lee081847@gmail.com>
2025-07-07 15:43:22 +0900

    sampling code

--- data_sampling/data_sampling/generate_data.py
+++ data_sampling/data_sampling/generate_data.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.logging import get_logger
 from rclpy.node import Node
+from ament_index_python.packages import get_package_share_directory
 
 # Pinocchio (FK)
 import pinocchio as pin
@@ -15,9 +16,18 @@ import trimesh
 from data_sampling.canadarm_wrapper import CanadarmWrapper
 from data_sampling.mesh_manager import MeshManager
 
+# Python3
+import os
+
 class SamplingNode(Node):
     def __init__(self):
         super().__init__("canadarm_jsdf_sampling")
+
+        package_name = "data_sampling"
+        self.data_path = os.path.join(get_package_share_directory(package_name), "data")
+        if not os.path.exists(self.data_path):
+            os.makedirs(self.data_path)
+
 
         self.robot = CanadarmWrapper()
         self.mesh_manager = MeshManager()
@@ -25,20 +35,87 @@ class SamplingNode(Node):
         self.q = np.zeros((self.robot.robot.nq))
         self.v = np.zeros((self.robot.robot.nv))
 
+        self.n_inner_pts = 25
+        self.n_far_pts = 25
+        self.n_close_pts = 25
+        self.n_zero_pts = 25
+
+        self.iter1 = 0
+        self.iter2 = 0
+        self.iter3 = 0
+        
+        self.inner_pts_array = np.empty((3,0))
+        self.closed_pts_array = np.empty((3,0))
+        self.far_pts_array = np.empty((3,0))
+
+        self.inner_dist_array = np.empty((8,0))
+        self.closed_dist_array = np.empty((8,0))
+        self.far_dist_array = np.empty((8,0))
+        self.inner_dist_array = np.empty((8,0))
+
+
+
+        self.is_sampling = False
+
+        self.total_pts_array = np.empty((3,0))
+
 
 def main(args=None):
     rclpy.init(args=args)
     node = SamplingNode()
     try:
         while rclpy.ok():
+            # Sampling Points
+            
             node.robot.computeAllTerms(node.q, node.v)
             link_list = node.robot.get_all_link_position()
             node.mesh_manager.update_all_meshes(link_list)
-            
-    except KeyboardInterrupt:
-        node.mesh_manager.visualize_all_meshes()
 
-            
+            node.get_logger().info(f"Len : {len(node.mesh_manager.mesh_list)}")
+            for i, mesh in enumerate(node.mesh_manager.mesh_list):
+                while True:
+                    point = np.random.rand(3)
+                    sample_pts = mesh.far_box.sampling_in_box(point)
+                    if mesh.box.check_in_box(sample_pts):
+                        if node.iter1 < node.n_inner_pts:
+                            node.inner_pts_array = np.hstack((node.inner_pts_array, sample_pts.reshape(3,1)))
+                            node.iter1 +=1
+                    elif mesh.closed_box.check_in_box(sample_pts):
+                        if node.iter2 < node.n_close_pts:
+                            node.closed_pts_array = np.hstack((node.closed_pts_array, sample_pts.reshape(3,1)))
+                            node.iter2 +=1
+
+                    else:
+                        if node.iter3 < node.n_far_pts:
+                            node.far_pts_array = np.hstack((node.far_pts_array, sample_pts.reshape(3,1)))
+                            node.iter3 +=1
+
+                    if node.iter1 >= node.n_inner_pts and node.iter2 >= node.n_close_pts and node.iter3 >= node.n_far_pts:
+                        node.total_pts_array = np.hstack((node.total_pts_array, node.inner_pts_array))
+                        node.total_pts_array = np.hstack((node.total_pts_array, node.closed_pts_array))
+                        node.total_pts_array = np.hstack((node.total_pts_array, node.far_pts_array))
+                        node.iter1 = node.iter2 = node.iter3 = 0
+                        node.inner_pts_array = np.empty((3,0))
+                        node.closed_pts_array = np.empty((3,0))
+                        node.far_pts_array = np.empty((3,0))
+                        
+                        if i == len(node.mesh_manager.mesh_list)-1:
+                            node.is_sampling = True
+                        break
+
+            if node.is_sampling:
+                np.savetxt(node.data_path + "/points.txt", node.total_pts_array.T, fmt="%.6f")
+                break
+
+    except KeyboardInterrupt:
+        node.get_logger().info("Node Cancled by User")
+
+    finally:
+        if node.is_sampling:
+            points = np.loadtxt(node.data_path + "/points.txt")
+            node.mesh_manager.visualize_all_meshes_with_sample(points)
+        else:
+            node.mesh_manager.visualize_all_meshes()
     node.destroy_node()
     rclpy.shutdown()
 
--- data_sampling/data_sampling/mesh_manager.py
+++ data_sampling/data_sampling/mesh_manager.py
@@ -11,6 +11,50 @@ import trimesh
 from rclpy.logging import get_logger
 from ament_index_python.packages import get_package_share_directory
 
+class box:
+    def __init__(self, V, scale_factor):
+        self.xmin = np.min(V[:,0])
+        self.xmax = np.max(V[:,0])
+        self.ymin = np.min(V[:,1])
+        self.ymax = np.max(V[:,1])
+        self.ymin = np.min(V[:,2])
+        self.ymax = np.max(V[:,2])
+
+        self.scale_factor = scale_factor
+    
+    def update_box(self, V):
+        self.xmin = np.min(V[:,0])
+        self.xmax = np.max(V[:,0])
+        self.ymin = np.min(V[:,1])
+        self.ymax = np.max(V[:,1])
+        self.zmin = np.min(V[:,2])
+        self.zmax = np.max(V[:,2])
+
+    def scaling(self):
+        del_x = self.xmax - self.xmin
+        del_y = self.ymax - self.ymin
+        del_z = self.zmax - self.zmin
+
+        self.xmin = self.xmin - self.scale_factor * del_x
+        self.xmax = self.xmax + self.scale_factor * del_x
+        self.ymin = self.ymin - self.scale_factor * del_y
+        self.ymax = self.ymax + self.scale_factor * del_y
+        self.zmin = self.zmin - self.scale_factor * del_z
+        self.zmax = self.zmax + self.scale_factor * del_z
+
+    def sampling_in_box(self, random):
+        x = self.xmin + (self.xmax - self.xmin) * random[0]
+        y = self.ymin + (self.ymax - self.ymin) * random[1]
+        z = self.zmin + (self.zmax - self.zmin) * random[2]
+
+        return np.array([x, y, z])        
+
+    def check_in_box(self, point):
+        x, y, z = point
+        return (self.xmin <= x <= self.xmax and self.ymin<=y<=self.ymax and self.zmin<=z<=self.zmax)
+    
+
+
 class mesh:
     def __init__(self, link_id, link_name, F, V):
         self.link_id = link_id
@@ -18,14 +62,28 @@ class mesh:
         self.init_V = V.copy()
         self.updated_V = self.init_V.copy()
         self.F = F
+
+        self.box = box(self.updated_V, 0.0)
+        self.closed_box = box(self.updated_V, 0.1)
+        self.far_box = box(self.updated_V, 1.0)
         
 
     def update_mesh(self, rot_mat, trans):
         for i in range(self.init_V.shape[0]):
             self.updated_V[i,:] = (rot_mat @ self.init_V[i,:].T).T + trans
 
+        self.box.update_box(self.updated_V)
+        self.closed_box.update_box(self.updated_V)
+        self.far_box.update_box(self.updated_V)
+        self.closed_box.scaling()
+        self.far_box.scaling()
+        
+
     def return_mesh(self):
         return trimesh.Trimesh(vertices=self.updated_V, faces=self.F, process=False)
+
+
+
 
 class MeshManager:
     def __init__(self):
@@ -70,6 +128,40 @@ class MeshManager:
     def visualize_all_meshes(self):
         scene = trimesh.Scene()
         
+        for m in self.mesh_list:
+            mesh_obj = m.return_mesh()
+            scene.add_geometry(mesh_obj, node_name=f"{m.link_id}_{m.link_name}")
+        
+        scene.show()
+
+    def visualize_all_meshes_with_sample(self, points):
+        scene = trimesh.Scene()
+
+        # 포인트를 8개로 나누기
+        N = points.shape[0]
+        group_size = N // 8
+
+        colors = np.zeros((N, 4), dtype=np.uint8)  # RGBA
+
+        color_list = [
+            [255, 0, 0, 255],     # 빨강
+            [0, 255, 0, 255],     # 초록
+            [0, 0, 255, 255],     # 파랑
+            [255, 255, 0, 255],   # 노랑
+            [0, 255, 255, 255],   # 시안
+            [255, 0, 255, 255],   # 마젠타
+            [128, 128, 128, 255], # 회색
+            [255, 128, 0, 255]    # 주황
+        ]
+
+        for i in range(8):
+            start = i * group_size
+            end = (i + 1) * group_size if i < 7 else N  # 마지막 그룹은 나머지까지
+            colors[start:end] = color_list[i]
+
+        cloud = trimesh.points.PointCloud(points, colors=colors)
+
+        scene.add_geometry(cloud)
         for m in self.mesh_list:
             mesh_obj = m.return_mesh()
             scene.add_geometry(mesh_obj, node_name=f"{m.link_id}_{m.link_name}")
